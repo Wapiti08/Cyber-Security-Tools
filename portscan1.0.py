363827274a1e55258c69dddd21ecb4d3f73eb3ea
wapiti08 <tzrzhuoran@163.com>
2018-08-11 20:00:32 +0800

    Add files via upload
    simply port-scan tool

---
+++ portscan1.0.py
@@ -0,0 +1,67 @@
+import optparse
+#the libirary above is used to analyse the 
+#command at the terminal
+import socket
+from socket import *
+
+#define the connection scan
+def connScan(tgtHost,tgtPort):
+    try:
+        #change the ip into ipv4
+        sockfd=socket(AF_INET,SOCK_STREAM)
+        sockfd.settimeout(5)
+        Address=(tgtHost,tgtPort)
+        sockfd.connect(Address)
+        # s.sendall(b'hello\r\n\r\n')
+        # results = s.recv(100)
+        sockfd.send(b'ViolentPython\r\n')
+        # #set the timeout
+        results=sockfd.recv(100)
+        print('[+]%d/tcp open'%tgtPort)
+        print('[+]'+str(results))
+        # print('[+]%s的%3s端口:打开' % (tgtHost,tgtPort)) #若可以建立连接，表示此端口是打开的
+        sockfd.close()
+    except:
+        print('[-]%d/tcp closed',tgtPort)
+
+#get the ip and the list of port
+def portScan(tgtHost,tgtPorts):
+    try:
+        #get the ip of the host
+        tgtIP=gethostbyname(tgtHost)   
+    except:
+        print("[-] Cannot resolve '%s':Unknown host"%tgtHost)
+        return
+    try:
+        #get the list of hostname,address,alien name
+        tgtName=gethostbyaddr(tgtIP)
+        
+        print('\n[+] Scan Results for:'+tgtName[0])
+    except:
+        print('\n[+] Scan Results for:'+tgtIP)
+    setdefaulttimeout(5)
+    for tgtPort in tgtPorts:
+
+        print('Scanning port'+tgtPort)
+        connScan(tgtHost,int(tgtPort))
+
+#the first step:create the list for IP,PORT,get from the host
+def main():
+    #create the paramatar analysis example
+    #optparse.OptionParser([usage message])
+    parser=optparse.OptionParser('usage %prog -H'+'<target host> -p <target port>')
+    #hostname equals to the ip address
+    parser.add_option('-H',dest='tgtHost',type='string',help='specify target host')
+    #port
+    parser.add_option('-p',dest='tgtPort',type='string',help='specify target port[s] separated by comma')
+    (options,args)=parser.parse_args()
+    tgtHost=options.tgtHost
+    tgtPorts=str(options.tgtPort).split(',')
+    if(tgtHost==None)|(tgtPorts[0]==None):
+        print('[-]You must specify a target host and port[s].')
+        exit(0)
+    portScan(tgtHost,tgtPorts)
+
+if __name__=='__main__':
+    main()
+
