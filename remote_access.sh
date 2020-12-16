f4e7dfe2cd6713c0d6cf3ee84fc91680040b8bb5
Wapiti08 <tzrzhuoran@gmail.com>
2020-12-16 20:59:31 +0000

    Update remote_access.sh

--- remote_access.sh
+++ remote_access.sh
@@ -3,5 +3,5 @@
 # nmap -p 3389 xxx.xxx.xxx.1-254 -oG remote_host.txt
 
 for ip in $(cat remote_host.txt | grep open | cut -d " " -f2); 
-	do rdesktop $ip -u offsec -p lab -g 1024x768 -x 0X80; 
+	do rdesktop $ip -u xxx -p xxx -g 1024x768 -x 0X80; 
 done
